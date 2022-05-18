916bd44a857ee13193318b72b7c341df6836da5f
Diego Carapia da Costa <diego.carapia@ufba.br>
2022-05-18 13:28:37 -0300

    form algoritmo escalonador

---
+++ main.js
@@ -0,0 +1,15 @@
+function FIFO(nProcessos, quantum) {
+    
+}
+
+function SJF(nProcessos, quantum) {
+
+}
+
+function RoundRobin(nProcessos, quantum) {
+    
+}
+
+function EDF(nProcessos, quantum) {
+    
+}
