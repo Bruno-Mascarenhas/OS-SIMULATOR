922269d9e503341a94b7d775e6b6e146720e1b77
Diego Carapia da Costa <diego.carapia@ufba.br>
2022-05-20 15:30:16 -0300

    pegando valores do formulario

--- src/templates/input.js
+++ src/templates/input.js
@@ -1,8 +1,17 @@
 const n_processos = document.getElementById('n-processos')
 n_processos.addEventListener('change', (e) => {
-    document.getElementById("processos").innerHTML="";
+    document.getElementById("processos").innerHTML = "";
     createAllFormProcess(e.target.value)
 });
+
+document.getElementById('formInput').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let data = Array.from(document.querySelectorAll('#formInput input')).reduce((acc, input) => ({ ...acc, [input.name]: input.value }), {});
+    let algoritimo = document.getElementById('algoritimo')
+    let value = algoritimo.options[algoritimo.selectedIndex].value;
+    console.log(value)
+    console.log(data)
+})
 
 function createAllFormProcess(n_processos) {
     for (i = 1; i <= n_processos; i++) {
