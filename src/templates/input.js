9c52c063415e9ebda3a68abf6317f33d40062c09
Diego Carapia da Costa <diego.carapia@ufba.br>
2022-05-20 15:53:10 -0300

    ...

--- src/templates/input.js
+++ src/templates/input.js
@@ -11,6 +11,7 @@ document.getElementById('formInput').addEventListener('submit', (e) => {
     let value = algoritimo.options[algoritimo.selectedIndex].value;
     console.log(value)
     console.log(data)
+    start();
 })
 
 function createAllFormProcess(n_processos) {
@@ -26,3 +27,76 @@ function createFormProcess(n_processo) {
     form.classList.add("m-2", "p-4", "shadow");
     container.appendChild(form)
 }
+
+// Animação
+function start() {
+    // Variaveis do sistema
+    let nProcessos = 4;
+    const animatioDelay = 0.5;
+
+    let time = [
+        [1, 'green'],
+        [1, 'green'],
+        [1, 'red'],
+        [2, 'green'],
+        [2, 'green'],
+        [3, 'green'],
+        [3, 'green'],
+        [3, 'green'],
+        [3, 'red'],
+        [4, 'green'],
+    ]
+
+    // ************************************************
+
+    let tempoTotal = time.length;
+    let tempoAtual = 0;
+
+    let myInterval = setInterval(animation, animatioDelay * 1000);
+    createTable();
+
+    function animation() {
+        tempoAtual += 1;
+
+        changeColor(time[tempoAtual - 1][0], tempoAtual, time[tempoAtual - 1][1]);
+
+        if (tempoAtual == tempoTotal) {
+            clearInterval(myInterval);
+        }
+    }
+
+    function createTable() {
+        for (i = 1; i <= nProcessos; i++) {
+            createRow(i);
+        }
+    }
+
+    function createRow(nProcesso) {
+        let table = document.getElementById("table");
+        let tableRow = document.createElement("tr");
+        let tableHead = document.createElement("th");
+        tableHead.innerHTML = nProcesso;
+        tableRow.append(tableHead);
+        for (j = 1; j <= tempoTotal; j++) {
+            let tableCell = document.createElement("td");
+            tableCell.setAttribute("id", nProcesso + "." + j);
+            tableRow.appendChild(tableCell);
+            table.appendChild(tableRow);
+        }
+    }
+
+    function changeColor(nProcesso, tempo, cor) {
+        document.getElementById(nProcesso + "." + tempo).style.backgroundColor = cor;
+        // document.getElementById("1.1").style.borderRightColor = "pink";
+    }
+
+    // const teste = document.querySelector('.teste')
+    // fetch('test.html')
+    // .then(res=>res.text())
+    // .then(data=>{
+    //     teste.innerHTML = data
+    // })
+}
+
+
+
