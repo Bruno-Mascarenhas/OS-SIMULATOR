ffda79394b5733936bc7dc182f5f1d2923e47190
Bruno-Mascarenhas <brunomasck@gmail.com>
2022-05-30 12:36:22 -0300

    edf and roundrobin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Process from './process/process.js';
+import PriorityQueue from './utils/priorityqueue.js';
 
 //First In FIrst Out
 function FIFO(processes) {
@@ -147,80 +148,99 @@ function SJF(processes) {
 }
 
 function RoundRobin(processes, quantum, overload) {
-    /*
-    processes = array da estrutura de dados process
-    */
-    
+    //Roda o processo até o valor do quantum do sistema
+    let sys_time = 0;
+    let processes_time = new Array(processes.length).fill(0).map((x) => new Array().fill(0))
+    let processes_bkp = processes.map((x) => x);
+
+    while(processes.length > 0) {
+        let process = processes.shift();
+
+        if(process.arrive > sys_time){
+            processes.push(process);
+            let find = 0;
+            for(let i=0; i<processes.lenght; i++){
+                if(processes[i].arrive <= sys_time){
+                    find = 1;
+                }
+            }
+            if(find == 0){
+                let tmp = 999999999;
+                for(let i=0; i<processes.length; i++){
+                    tmp = min(tmp, processes[i].arrive);
+                }
+                sys_time = tmp;
+            }
+        }
+        else if(process.executionTime > quantum) {
+            process.executionTime -= quantum;
+            processes_time[process.id].push([sys_time, sys_time + quantum]);
+            sys_time += quantum + overload;
+            processes.push(process);
+        } else {
+            processes_time[process.id].push([sys_time, sys_time + process.executionTime]);
+            sys_time += process.executionTime;
+        }
+    }
+
+    let turnaround = 0;
+    processes_time.forEach((times,i) => {
+        //console.log(`times ${i} = `,times)
+        //console.log(times[times.length-1][1], processes_bkp[i].arrive), '\n';
+        turnaround += times[times.length-1][1] - processes_bkp[i].arrive;
+    });
+
+    turnaround = turnaround / processes_bkp.length;
+    console.table(processes_time)
+    console.log(turnaround / processes_bkp.length);
+    return turnaround
 }
 
 function EDF(processes, quantum, overload) {
-    //Nesse caso o quantum e o overload são necessários.
+    //Roda os processos de acordo o deadline e vai adicionando o quantum
+    let sys_time = 0;
+    let processes_time = new Array(processes.length).fill(0).map((x) => new Array().fill(0))
+    let processes_bkp = processes.map((x) => x);
 
-    console.log("Processos antes da ordenação");
-    console.table(processes); //printa em uma tabela os processos recebidos
+    let pq = new PriorityQueue((x, y) => x.deadline > y.deadline)
+    processes.forEach((x) => pq.push(x))
 
-    //Ordena os processos por deadline.
-    processes.sort((x, y) => {
-        return x.deadline - y.deadline;
-    });
+    while(processes.length > 0) {
+        let process = processes.shift();
 
-    console.log("Processos depois da ordenação");
-    console.table(processes);
-
-    //variáveis para a posição inicial e final de cada processo
-    //processTime vai precisar serr uma amtriz pois os processos são executados mais de uma vez
-    var processTime = [];
-    var aux1, aux2;
-    //parciais para calcular o turnaround médio
-    var partial = [];
-    //variável com as deadlines de cada processo, já em ordem por deadline. Preciso no gráfico.
-    var deadlines = [];
-
-    for(var i = 0; i<processes.length; i++){ // preenche o vetor de deadlines
-        deadlines[i] = processes[i].deadline;
-    }
-
-    if(overload == 0) { //Quando não há sobrecarga, o quantum nem importa
-        for(var i = 0; i<processes.length; i++){
-            if(i == 0){
-                aux1 = processes[i].arrive;
-                aux2 = aux1 + processes[i].executionTime;
-                partial.push(processes[i].executionTime); //adiciona o turnaround do primeiro processo no array
-                processTime.push([aux1,aux2]) //adiciona as cordenadas do primeiro processo no array
-            } else { 
-                if(processes[i].arrive <= aux2) { //quando o proximo processo está em espera
-                    aux1 = aux2;
-                    aux2 = aux1 + processes[i].executionTime;
-                    //adiciona o turnaround do processo no array
-                    partial.push(processes[i].executionTime + partial[i-1]);
-                } else { //quando o processo anterior termina antes do atual chegar
-                    aux1 = processes[i].arrive;
-                    aux2 = aux1 + processes[i].executionTime;
-                    //adiciona o turnaround do processo no array
-                    partial.push(processes[i].executionTime + partial[i-1]);
+        if(process.arrive > sys_time){
+            processes.push(process);
+            let find = 0;
+            for(let i=0; i<processes.lenght; i++){
+                if(processes[i].arrive <= sys_time){
+                    find = 1;
                 }
-                //adicionando coordenadas do processo no array
-                processTime.push([aux1,aux2]);
+            }
+            if(find == 0){
+                let tmp = 999999999;
+                for(let i=0; i<processes.length; i++){
+                    tmp = min(tmp, processes[i].arrive);
+                }
+                sys_time = tmp;
             }
         }
-    } else {
-        //?
+        else if(process.executionTime > quantum) {
+            process.executionTime -= quantum;
+            processes_time[process.id].push([sys_time, sys_time + quantum]);
+            sys_time += quantum + overload;
+            processes.push(process);
+        } else {
+            processes_time[process.id].push([sys_time, sys_time + process.executionTime]);
+            sys_time += process.executionTime;
+        }
     }
-
-    //soma todos os valores do array partial
-    var sum = partial.reduce(function(sum,i){
-        return sum + i;
-    });
-
-    //turnaround médio
-    var turnaround = sum/processes.length;
-
-    console.log("Partial: ",partial);
-    console.log("Sum: ",sum);
-    console.log("Process Time: ",processTime);
-    console.log("Trunaround: ",turnaround);
-    console.log("Deadlines: ",deadlines);
-
 }
+
+let a = new Process(0,0,2,20,1)
+let b = new Process(1,1,3,20,1)
+let c = new Process(2,2,1,20,1)
+let d = new Process(3,3,4,20,1)
+
+RoundRobin([a,b,c,d],10,0)
 
 export {FIFO,SJF,RoundRobin,EDF}
